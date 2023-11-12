9ffcfb1f44886fef8f114b8043afbdeb0e67e841
Jackson Cooke <jackson.cooke@duke.edu>
2023-11-12 18:21:45 -0500

    new classes added

--- src/InteractivePercolationVisualizer.java
+++ src/InteractivePercolationVisualizer.java
@@ -6,7 +6,6 @@
  * @author Josh Hug
  * @author Owen Astrachan
  ******************************************************************************/
-//HE LICK THE HONEY UP MY PERCOLATER
 import java.awt.Font;
 
 public class InteractivePercolationVisualizer {

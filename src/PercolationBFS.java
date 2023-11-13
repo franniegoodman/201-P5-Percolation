d4e632bbae3880228ead063bdb5fb8e4d96f677c
Frannie Goodman <frannie.goodman@duke.edu>
2023-11-12 20:44:15 -0500

    time for ya percalator

--- src/PercolationBFS.java
+++ src/PercolationBFS.java
@@ -8,30 +8,33 @@ public class PercolationBFS extends PercolationDefault{
     }
 
     @Override
-    protected void search(int startRow, int startCol) {
-        if (isFull(startRow, startCol) || !isOpen(startRow, startCol)) {
+    protected void search(int row, int col) {
+        if (!inBounds(row, col)) return;
+        
+        if (isFull(row, col) || !isOpen(row, col)) {
             return;
         }
-
-        int[] rowDelta = {-1, 1, 0, 0};
-        int[] colDelta = {0, 0, -1, 1};
-
+        
         Queue<int[]> queue = new LinkedList<>();
-        myGrid[startRow][startCol] = FULL;
-        queue.add(new int[]{startRow, startCol});
-
+        
+        myGrid[row][col] = FULL;
+        queue.add(new int[] {row, col});
+        
         while (!queue.isEmpty()) {
-            int[] coordinates = queue.remove();
-            int row = coordinates[0];
-            int col = coordinates[1];
-
-            for (int k = 0; k < rowDelta.length; k++) {
-                int newRow = row + rowDelta[k];
-                int newCol = col + colDelta[k];
-
-                if (inBounds(newRow, newCol) && myGrid[newRow][newCol] == FULL) {
-                    queue.add(new int[]{newRow, newCol});
+            int[] cell = queue.poll();
+            int myRow = cell[0];
+            int myCol = cell[1];
+            
+            int[] deltaRow = {-1, 0, 0, 1};
+            int[] deltaCol = {0, -1, 1, 0};
+            
+            for (int k = 0; k < deltaRow.length; k++) {
+                int newRow = myRow + deltaRow[k];
+                int newCol = myCol + deltaCol[k];
+                
+                if (inBounds(newRow, newCol) && isOpen(newRow, newCol) && !isFull(newRow, newCol)) {
                     myGrid[newRow][newCol] = FULL;
+                    queue.add(new int[] {newRow, newCol});
                 }
             }
         }
